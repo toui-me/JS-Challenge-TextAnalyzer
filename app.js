6f1bf7929562b96a4731a75a0b054487afa5fda8
Ian <iancover@gmail.com>
2017-05-12 09:59:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // your code here!
+
+// create listener for submit button
+// count words in <textarea> element
+// do a unique word count, words that are not repeated
+// average word length in characters
+
+
+$('button').click(function(event) {});
+
+function wordCounter() {}
+
+function wordUniques() {}
+
+function wordLengthAvg() {}
+
+var conText = $('textarea').val();
+// see how to extract that content to be counted
+
+var wordCount = conText.split('');
+// to split the string into array to count
+
+var charCount = wordCount.map(''.split) // I think this is the correct one
+return charCount.split
+
+for (var i=0;i<conText.length;i++) {
+// var =
+// return var;
+}
+// to count total words, unique words, and letters
+
+if () {
+	// for uniques
+}
